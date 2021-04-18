fbd699fd0d3d75bb271c68c8170dbaf9fb9ff6a0
Den-droid <amerscan8@gmail.com>
2021-04-18 15:25:01 +0300

    First commit

---
+++ src/com/example/Node.java
@@ -0,0 +1,9 @@
+package com.example;
+
+class Node {
+    int data; // holds the key
+    Node parent; // pointer to the parent
+    Node left; // pointer to left child
+    Node right; // pointer to right child
+    byte color; // 1 . Red, 0 . Black
+}
